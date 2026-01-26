03670216dc258fa6cda9031bb9f92054d088a39a
ayushL2007 <ayushlahiri2008@gmail.com>
2026-01-26 12:12:01 +0530

    Sitemap modified

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -3,7 +3,7 @@ class SitemapController < ApplicationController
         # Path to the file in your assets folder
         file_path = Rails.root.join('app', 'assets', 'stylesheets', 'sitemap.xml')
 
-        if File.exists?(file_path)
+        if File.exist?(file_path)
             sitemap_content = File.read(file_path)
             
             send_data sitemap_content, 
